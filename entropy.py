99f6fd2b2baf6826e464e20346dae2a8e5305132
Josh Roppo <joshroppo@gmail.com>
2012-01-27 23:10:48 -0800

    Added base code file

---
+++ entropy.py
@@ -0,0 +1,39 @@
+import os, sys, pygame, Box2D 
+
+class Game:
+  def __init__(self):
+    pygame.init()
+
+    self.screen_size = [512, 512]
+
+    self.screen = pygame.display.set_mode(self.screen_size)
+    self.clock = pygame.time.Clock()
+    pygame.display.set_caption('Entropy')
+
+    self.backgd = pygame.Surface(self.screen.get_size())
+    back_color = [128,128,128]
+
+    self.frames = 0
+    self.show = pygame.sprite.RenderClear()
+
+  def run_loop():
+    
+    go = True
+    black = [0,0,0]
+    while go:
+      go = True
+      self.clock.tick(60)
+      self.screen.fill(black)
+
+      pygame.event.pump()
+      key = pygame.key.get_pressed()
+
+      pygame.display.flip()
+
+
+def main():
+  game = Game()
+
+
+if __name__ == '__main__':
+  main()
