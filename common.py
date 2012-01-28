cca842fae73beb8fa94133d88cb1d5d6bc2dc29a
Michael Dougherty <maackle.d@gmail.com>
2012-01-28 05:12:16 -0800

    started adding bodies

--- common.py
+++ common.py
@@ -1,11 +1,17 @@
 import pygame, Box2D
 import math
-import entropy
-from game_state import *
 from Box2D import *
+
+vec = b2Vec2
+
+import physics
+from physics import world
+
+def toScreen(v):
+  import game_state
+  return game_state.GameState.current.toScreen(v)
 
 # class vec(b2Vec2, list):
 #   def tup(self):
 #     return (self.x, self.y)
 
-vec = b2Vec2
